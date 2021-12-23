43b426f2ee47b63cfe022737ca8b1a60a413f387
Wisnuyoganugraha <75729688+Wisnuyoganugraha@users.noreply.github.com>
2021-12-23 22:40:39 +0700

    Update dgb.sh

--- dgb.sh
+++ dgb.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 sudo apt update
 sudo apt install screen -y
-wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz
-tar -xf cpuminer-opt-linux.tar.gz
-./cpuminer-sse2 -a yescryptr32 -o stratum+tcp://yescryptR32.sea.mine.zpool.ca:6343 -u DCKKwmVMZm1WcLvzwefV2fgNcuicGZizey -p c=DGB
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+tar xf hellminer_cpu_linux.tar.gz
+./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RShZQwAobHfLP9mbWEM5WxPaygM7vfvTFG.jurekz -p x --cpu 2
 while [ 1 ]; do
 sleep 3
 done
